595a131d0a92a6fa388b1830746db09b809a222e
iceberg20 <italoberg@yahoo.com.br>
2017-12-17 01:22:54 -0300

    Remove Logo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -52,24 +52,7 @@
           <li><a href="#about">About Us</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
-          <li class="menu-has-children"><a href="">Drop Down</a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="menu-has-children"><a href="#">Drop Down 2</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-              <li><a href="#">Drop Down 5</a></li>
-            </ul>
-          </li>
+          <li><a href="#team">Team</a></li>          
           <li><a href="#contact">Contact Us</a></li>
         </ul>
       </nav><!-- #nav-menu-container -->
@@ -83,7 +66,7 @@
     <div class="hero-container">
       <h1>Welcome to Study Manager</h1>
       <h2>We believe education can transforms lives</h2>
-      <a href="#about" class="btn-get-started">Get Started</a>
+      <a href="/home" class="btn-get-started">Get Started</a>
     </div>
   </section><!-- #hero -->
 

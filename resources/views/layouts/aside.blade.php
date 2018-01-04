02ef724a53cbe67a1e90f649786a1c951c1f2553
iceberg20 <italoberg@yahoo.com.br>
2018-01-04 11:41:47 -0200

    Asside reformulation

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -1,15 +1,16 @@
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
           <div class="sidebar-module sidebar-module-inset">
-            <h4>About</h4>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod.</p>
+            <h4>To think</h4>
+            <p>"The future belongs to those who prepare for it today."
+            <em> -Malcon X  </em></p>
           </div>
           <div class="sidebar-module">
           <div class="sidebar-module">
             <h4>Elsewhere</h4>
             <ol class="list-unstyled">
-              <li><a href="#">GitHub</a></li>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Facebook</a></li>
+              <li><a href="http://www.csc.edu/learningcenter/study/studymethods.csc">Learningcenter</a></li>
+              <li><a href="https://www.academictips.org/acad/">Academics Tips</a></li>
+              <li><a href="http://www.youthcentral.vic.gov.au/studying-training/studying-tips-resources/top-10-study-tips">Studying Training</a></li>
             </ol>
           </div>
         </aside><!-- /.blog-sidebar -->
--- resources/views/planning/planning.blade.php
+++ resources/views/planning/planning.blade.php
@@ -2,12 +2,40 @@
 
 @section('content')
 
-	<h1> Plans </h1>
+	<h1 id="title-plan"> Plans </h1>
 
 
-	<p class="t2"> Gold: Concurso ITEP </p>
-	<p class="t2"> Study Time: 4h/dia </p>
-	<p class="t2"> Disciplinas: Informática, Português. </p>
+
+<div class="card">
+  <h3 class="card-header">Concurso ITEP</h3>
+  <div class="container incard">
+	  <div class="card-block">
+		<p> <span class="t2"> Study Time: </span> 4h/dia </p> 
+		<p> <span class="t2"> Disciplinas: </span> Informática, Português. </p>
+		<p> <span class="t2"> Final Date: </span> 06/01/2017 </p>
+	    <a style="margin-bottom: 30px;" href="#">Edit</a>
+	  </div>
+	</div>
+</div>
+
+<div class="card">
+  <h3 class="card-header">Prova de Matemática</h3>
+  <div class="container incard">
+	  <div class="card-block">
+		<p> <span class="t2"> Study Time: </span> 4h/dia </p> 
+		<p> <span class="t2"> Disciplinas: </span> Informática, Português. </p>
+		<p> <span class="t2"> Final Date: </span> 06/01/2017 </p>
+	    <a style="margin-bottom: 30px;" href="#">Edit</a>
+	  </div>
+	</div>
+</div>
+
+</br>
+</br>
+<button type="button" class="btn btn-default" aria-label="Left Align">
+	Add Plan
+  <span class="fa fa-plus" aria-hidden="true"></span>
+</button>
 	
 
 @endsection
